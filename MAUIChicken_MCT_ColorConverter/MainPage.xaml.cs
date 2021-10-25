0f52c9131c7aee3055f190339c01a7dfa946d965
JSturla <jms0210@gmail.com>
2021-10-25 20:24:12 -0300

    Format, new components on the XAMl

--- MAUIChicken_MCT_ColorConverter/MainPage.xaml.cs
+++ MAUIChicken_MCT_ColorConverter/MainPage.xaml.cs
@@ -8,57 +8,61 @@ using Microsoft.Maui.Graphics;
 
 namespace MAUIChicken_MCT_ColorConverter
 {
-	public partial class MainPage : ContentPage
-	{
-		readonly Dictionary<string, Color> _colors = typeof(Colors)
-	   .GetFields(BindingFlags.Static | BindingFlags.Public)
-	   .ToDictionary(c => c.Name, c => (Color)(c.GetValue(null) ?? throw new InvalidOperationException()));
+    public partial class MainPage : ContentPage
+    {
+        readonly Dictionary<string, Color> _colors = typeof(Colors)
+       .GetFields(BindingFlags.Static | BindingFlags.Public)
+       .ToDictionary(c => c.Name, c => (Color)(c.GetValue(null) ?? throw new InvalidOperationException()));
 
-		int count = 0;
+        int count = 0;
 
-		public MainPage()
-		{
-			InitializeComponent();
-			myCollectionView.ItemsSource = _colors.Values.ToList();
-		}
-
-		private void OnCounterClicked(object sender, EventArgs e)
-		{
-			tintableImage.TintColor = _colors.Values.ToList()[new Random().Next(_colors.Count)];
-		}
-
-		private void Clear(object sender, EventArgs e)
+        public MainPage()
         {
-			tintableImage.TintColor = Colors.Transparent;
+            InitializeComponent();
+            myCollectionView.ItemsSource = _colors.Values.ToList();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            tintableImage.TintColor = _colors.Values.ToList()[new Random().Next(_colors.Count)];
+        }
+
+        private void Clear(object sender, EventArgs e)
+        {
+            tintableImage.TintColor = Colors.Transparent;
         }
 
         void TapGestureRecognizer_Tapped(System.Object sender, System.EventArgs e)
         {
-            try { 
-			Console.WriteLine("Event fired with " + e.ToString());
-			TappedEventArgs tappedEventArgs = e as TappedEventArgs;
-			Console.WriteLine((tappedEventArgs.Parameter as Color).ToString());
-			tintableImage.TintColor = tappedEventArgs.Parameter as Color;
-        }catch(Exception ex)
+            try
             {
-				Console.WriteLine("Ex: " + ex.Message);
+                Console.WriteLine("Event fired with " + e.ToString());
+                TappedEventArgs tappedEventArgs = e as TappedEventArgs;
+                Console.WriteLine((tappedEventArgs.Parameter as Color).ToString());
+                tintableImage.TintColor = tappedEventArgs.Parameter as Color;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ex: " + ex.Message);
 
             }
-			}
+        }
 
         void myCollectionView_SelectionChanged(System.Object sender, Microsoft.Maui.Controls.SelectionChangedEventArgs e)
         {
-			try {
-				Console.WriteLine("Type is ", e.GetType().ToString());
-				Console.WriteLine("Event fired with " + e.CurrentSelection.ToString());
-				Color selectedColor = (Color)e.CurrentSelection.FirstOrDefault();
-				Console.WriteLine(selectedColor.ToString());
-				tintableImage.TintColor =  selectedColor;
-			}catch (Exception ex)
-			{
-				Console.WriteLine("Ex: " + ex.Message);
+            try
+            {
+                Console.WriteLine("Type is ", e.GetType().ToString());
+                Console.WriteLine("Event fired with " + e.CurrentSelection.ToString());
+                Color selectedColor = (Color)e.CurrentSelection.FirstOrDefault();
+                Console.WriteLine(selectedColor.ToString());
+                tintableImage.TintColor = selectedColor;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ex: " + ex.Message);
 
-			}
-		}
+            }
+        }
     }
 }
